a0889e11dc36a95376263404e89f06925a9d7068
Cenker <cenkerkumlucali0@gmail.com>
2021-05-30 02:35:54 +0300

    the design has changed

--- src/app/components/favorite/favorite.component.ts
+++ src/app/components/favorite/favorite.component.ts
@@ -1,9 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr';
+import { element } from 'protractor';
 import { Favorite } from 'src/app/models/favorite';
 import { FavoriteDetails } from 'src/app/models/favoriteDetails';
+import { Product } from 'src/app/models/product';
+import { ProductDetail } from 'src/app/models/productDetail';
 import { AuthService } from 'src/app/services/auth.service';
 import { FavoriteService } from 'src/app/services/favorite.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-favorite',
@@ -12,33 +16,40 @@ import { FavoriteService } from 'src/app/services/favorite.service';
 })
 export class FavoriteComponent implements OnInit {
 
-  favorites:Favorite[]=[]
-  favoriteDetails:FavoriteDetails[]=[]
-  constructor(private favoriteService:FavoriteService,
-              private authService:AuthService,
-              private toastrService:ToastrService) { }
+  favorites: Favorite[] = []
+  favoriteDetails: FavoriteDetails[] = []
+  defaultImg = ""
+  products:Product
+  imageBasePath = environment.imageUrl
+  constructor(private favoriteService: FavoriteService,
+    private authService: AuthService,
+    private toastrService: ToastrService) { }
 
   ngOnInit(): void {
-    this.getDetails()
     this.getDetailsByUserId()
   }
 
-  getDetails(){
-  this.favoriteService.getAllDetails().subscribe((response)=>{
-    this.favorites = response.data
-  })
-  }
-  getDetailsByUserId(){
-    this.favoriteService.getDetailsByUserId(this.authService.getCurrentUserId()).subscribe((response)=>{
+  getDetailsByUserId() {
+    this.favoriteService.getDetailsByUserId(this.authService.getCurrentUserId()).subscribe((response) => {
       this.favoriteDetails = response.data
-      console.log(response.data);
-      
     })
-    }
+  }
+  getDetailsByFilteredAsc() {
 
-  delete(favorite:Favorite){
-    this.favoriteService.Delete(favorite).subscribe((response)=>{
+  let result:any = this.favoriteDetails.map(c=>c.productDetailDtos.find(c=>c.price)).sort((a,b) => a.price - b.price)//this.favoriteDetails.map(c=>c.productDetailDtos.find(c=>c.price)).sort((n1,n2) => n1.price - n2.price)
+  
+  this.favoriteDetails.push(result)
+  this.favoriteDetails.map(c=>c.productDetailDtos).push(result)  
+  console.log(this.favoriteDetails);
+  console.log(result);
+  
+  }
+  delete(favorite: Favorite) {
+    this.favoriteService.delete(favorite).subscribe((response) => {
       this.toastrService.success(response.message)
     })
+  }
+  addToCart(product:Product){
+    
   }
 }
