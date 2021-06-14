6ff11b8e59fb5cf02b740df6083a9ac52ba8e9a0
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-06-14 17:18:38 +0300

    user guard added

---
+++ src/app/guards/user.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class UserGuard implements CanActivate {
+  constructor(private authService: AuthService,
+    private toastrService: ToastrService,
+    private router: Router) { }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if (this.authService.getCurrentRoles() === "user") {
+        return true;
+      } else {
+        this.router.navigate(["/admin/product/add"])
+        this.toastrService.info("Sayfa bulunamadı")
+        return false
+      }
+  }
+  
+}
