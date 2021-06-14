b68f13e0d0a246d3a2363be4481e7ae704eed289
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-06-14 21:06:00 +0300

    design change for admin products

---
+++ src/app/components/admin/admin-products/admin-products.component.ts
@@ -0,0 +1,39 @@
+import { Product } from './../../../models/product';
+import { ToastrService } from 'ngx-toastr';
+import { ProductDetail } from 'src/app/models/productDetail';
+import { ProductService } from './../../../services/product.service';
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-admin-products',
+  templateUrl: './admin-products.component.html',
+  styleUrls: ['./admin-products.component.css']
+})
+export class AdminProductsComponent implements OnInit {
+  products:ProductDetail[]
+  product:ProductDetail
+  selectedProduct:ProductDetail=null
+  imageUrl=environment.imageUrl
+  constructor(private productService:ProductService,
+    private toastrService:ToastrService) { }
+
+  ngOnInit(): void {
+    this.getProducts()
+  }
+
+  getProducts(){
+    this.productService.getProductDetails().subscribe((response)=>{
+      this.products = response.data
+    })
+  }
+  deleteProduct(productDetail:ProductDetail){
+    this.productService.delete(productDetail).subscribe((response)=>{
+      this.toastrService.success(response.message)
+    })
+  }
+  setSelectedProduct(product:ProductDetail){
+    this.selectedProduct = product    
+  }
+ 
+}
