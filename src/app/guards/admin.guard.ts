406f6db74030776762ca900cf83cdbf1d276eebd
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-06-14 17:18:30 +0300

    admin guard added

---
+++ src/app/guards/admin.guard.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  constructor(private authService: AuthService,
+    private toastrService: ToastrService,
+    private router: Router) { }
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.authService.getCurrentRoles() === "admin") {
+      return true;
+    } else {
+      this.router.navigate(["/"])
+      this.toastrService.info("Sayfa bulunamadı")
+      return false
+    }
+
+  }
+
+}
