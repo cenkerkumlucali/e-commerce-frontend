661c6a3336ee1f8f2a098dc7eaf8c8db7977f8b6
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-06-14 17:17:50 +0300

    added products/add and login component path

--- src/app/components/admin/admin-routing.module.ts
+++ src/app/components/admin/admin-routing.module.ts
@@ -1,14 +1,20 @@
+import { AdminProductsComponent } from './admin-products/admin-products.component';
+import { AdminLoginComponent } from './admin-login/admin-login.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AdminComponent } from './admin.component';
 import { ProductAddComponent } from './product-add/product-add.component';
+import { AdminGuard } from 'src/app/guards/admin.guard';
 
 const routes: Routes = [
+
   {
-    path: "admin",
+    path: "",
     component: AdminComponent,
     children: [
-      { path: 'product/add', component: ProductAddComponent },
+      { path: 'product/add', component: ProductAddComponent, canActivate: [AdminGuard] },
+      { path: 'products', component: AdminProductsComponent, canActivate: [AdminGuard] },
+      { path: 'login', component: AdminLoginComponent },
     ],
   }
 ];
