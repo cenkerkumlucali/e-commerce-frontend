e7408fad94fe74ab883f92e9028297466353a941
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-06-15 18:29:45 +0300

    delete operation added

--- src/app/services/product-image.service.ts
+++ src/app/services/product-image.service.ts
@@ -1,3 +1,6 @@
+import { SingleResponseModel } from './../models/singleResponseModel';
+import { ListResponseModel } from './../models/listResponseModel';
+import { ProductImage } from './../models/productImage';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
@@ -8,9 +11,13 @@ import { ResponseModel } from '../models/responseModel';
   providedIn: 'root'
 })
 export class ProductImageService {
-apiUrl = environment.baseUrl
-  constructor(private httpClient:HttpClient) { }
-  add(image:FormData):Observable<ResponseModel>{
-    return this.httpClient.post<ResponseModel>(this.apiUrl+"productimage/add",image)
+  apiUrl = environment.baseUrl
+  constructor(private httpClient: HttpClient) { }
+  
+  add(image: FormData): Observable<ResponseModel> {
+    return this.httpClient.post<ResponseModel>(this.apiUrl + "productimage/add", image)
+  }
+  delete(image:ProductImage){
+    return this.httpClient.post(this.apiUrl + "productimage/delete" , image)
   }
 }
