63afab1a81565107f901811d1732e3301e090150
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-06-18 19:48:19 +0300

    profil change design and removed password

--- src/app/components/profil/profil.component.ts
+++ src/app/components/profil/profil.component.ts
@@ -1,10 +1,14 @@
+import { UserImageService } from './../../services/user-image.service';
 import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { User } from 'src/app/models/user';
+import { UserDetail } from 'src/app/models/userDetail';
 import { AuthService } from 'src/app/services/auth.service';
 import { UserService } from 'src/app/services/user.service';
+import { environment } from 'src/environments/environment';
+import { UserImage } from 'src/app/models/userImage';
 
 
 @Component({
@@ -13,73 +17,117 @@ import { UserService } from 'src/app/services/user.service';
   styleUrls: ['./profil.component.css']
 })
 export class ProfilComponent implements OnInit {
-  profileForm:FormGroup;
-  email:string;
-  password:FormControl
-  user:User = new User()
-  status:string
-  currentUser:User
+  imageBaseUrl = environment.imageUrl
+  users: UserDetail
+  Image:string
+  defaultImg = "/images/NotFoundImageForUser.png"
+  profileForm: FormGroup;
+  email: string;
+  password: FormControl
+  user: User = new User()
+  status: string
+  currentUser: User
+  formData = new FormData();
   constructor(
-              private userService:UserService,
-              private formBuilder:FormBuilder,
-              private toastrService:ToastrService,
-              private authService:AuthService,
+    private userService: UserService,
+    private formBuilder: FormBuilder,
+    private toastrService: ToastrService,
+    private authService: AuthService,
+    private userImageService: UserImageService
   ) { }
 
   ngOnInit(): void {
     this.createProfileUpdateForm();
     this.email = localStorage.getItem('email')
     this.getUser();
+    this.getUserDetailsById()
   }
-
-  createProfileUpdateForm(){
-    this.profileForm = this.formBuilder.group({
-      id:this.user.id,
-      firstName:["",Validators.required],
-      lastName:["",Validators.required],
-      email:["",Validators.required],
-      password:["",Validators.required],
-      status:true
+  getUserDetailsById() {
+    this.userService.getUserDetailsById(this.authService.getCurrentUserId()).subscribe((response) => {
+      this.users = response.data[0]
+      this.Image = this.users.image.imagePath
     })
   }
-
-  getUser(){
-    if(this.email){
-      this.userService.getByEmail(this.email).subscribe(response=>{
+  createProfileUpdateForm() {
+    this.profileForm = this.formBuilder.group({
+      id: this.user.id,
+      firstName: ["", Validators.required],
+      lastName: ["", Validators.required],
+      email: ["", Validators.required],
+      status: true
+    })
+  }
+  getUser() {
+    if (this.email) {
+      this.userService.getByEmail(this.email).subscribe(response => {
         this.user = response
         if (response.status) {
           this.status = "Aktif"
-        }else{
+        } else {
           this.status = "Aktif değil"
         }
-      },responseError=>{
+      }, responseError => {
         this.toastrService.error(responseError.error)
       })
     }
   }
-
-  updateProfile(){
-    if(this.profileForm.valid){
-      let profilModel = Object.assign({},this.profileForm.value)
-      this.userService.profileUpdate(profilModel).subscribe(response=>{
-        this.toastrService.success(response.message)
-      },responseError=>{
-        this.toastrService.error(responseError.error)
-      });
-    }else{
+  updateProfile() {
+    if (this.profileForm.valid) {
+      if (this.users?.image?.imagePath?.length === 0) {
+        this.addImage()
+        
+      } else {
+        
+        let profilModel = Object.assign({}, this.profileForm.value)
+        this.userService.profileUpdate(profilModel).subscribe(response => {
+          this.updateImage()
+          this.toastrService.success(response.message)
+          setTimeout(() => window.location.reload(), 500)
+        }, responseError => {
+          if (responseError.error.Errors.length > 0) {
+            console.log(responseError.error.Errors)
+            for (let i = 0; i < responseError.error.Errors.length; i++) {
+              this.toastrService.error(responseError.error.Errors[i].ErrorMessage,
+              )
+            }
+          }
+        });
+      }
+    } else {
       this.toastrService.error("Formu boş bırakmayınız")
     }
   }
-  getCurrentCategoryClass() {
-    
-    return "list-group-item active"
-  
-}
-getProfilEditClass(){
-  if(!this.currentUser){
-    return "list-group-item active"
-  }else{
-    return "list-group-item "
+  /*User Image Operation Started*/
+  addImage() {
+
+    this.formData.append('userId', this.user.id.toPrecision())
+    this.userImageService.add(this.formData).subscribe((response) => { })
   }
-}
+  deleteImage(image: UserImage) {
+    this.userImageService.delete(image).subscribe((response) => { })
+  }
+  updateImage() {
+    this.formData.set('id', this.users.image.id.toPrecision())
+    this.formData.append('userId', this.user.id.toPrecision())
+    this.userImageService.update(this.formData).subscribe((response) => { })
+  }
+  onSelectFile(fileInput: any) {
+    for (let i = 0; i < fileInput.target.files.length; i++) {
+      let selectedFile = <File>fileInput.target.files[i];
+      this.formData.append('image', selectedFile)
+    }
+  }
+
+
+  /*User Image Operation Ended*/
+  getCurrentCategoryClass() {
+    return "list-group-item active"
+  }
+  getProfilEditClass() {
+    if (!this.currentUser) {
+      return "list-group-item active"
+    } else {
+      return "list-group-item "
+    }
+  }
 }
