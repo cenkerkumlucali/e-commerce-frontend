b3af29dd494ac93eede20ad5e1c4e08bfed15576
Cenker Kumlucalı <cenkerkumlucali0@gmail.com>
2021-07-08 18:01:02 +0300

    Fix

--- src/app/services/user-comment-image.service.ts
+++ src/app/services/user-comment-image.service.ts
@@ -3,6 +3,8 @@ import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 import {ResponseModel} from '../models/responseModel';
 import {environment} from 'src/environments/environment';
+import {ListResponseModel} from '../models/listResponseModel';
+import {UserCommentImage} from '../models/userCommentImage';
 
 @Injectable({
   providedIn: 'root'
@@ -18,4 +20,7 @@ export class UserCommentImageService {
     return this.httpClient.post<ResponseModel>(this.apiUrl + 'UserCommentImages/add', image);
   }
 
+  getImagesByCommentId(commentId: number): Observable<ListResponseModel<UserCommentImage>> {
+    return this.httpClient.post<ListResponseModel<UserCommentImage>>(this.apiUrl + 'UserCommentImages/getimagesbycommentid?commentId=', commentId);
+  }
 }
